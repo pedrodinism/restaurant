e55adbc04e7def54feac315c66fc089500813195
Pedro Marques <52702280+pedrodinism@users.noreply.github.com>
2026-04-09 15:51:39 +0200

    reverted changes

--- src/home.js
+++ src/home.js
@@ -1,6 +1,6 @@
 export function createHome() {
     const divName = document.createElement('div')
-    divName.innerText = 'Grandma Food 2'
+    divName.innerText = 'Grandma Food'
     divName.id = 'divName'
     return divName
 }
